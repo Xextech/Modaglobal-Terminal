a02a2f317353e3fb3bf20e9b78e0f96800c77638
Marta (Alex Bové) <marta@ioc.com>
2025-12-12 13:09:47 +0000

    Afegida funcionalitat de validació per evitar contactes en blanc

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('Projecte iniciat');
+
+function validarFormulari() {
+    let valor = "test"; // Simulació simple
+    if (valor === "") {
+        console.log("Error: El camp no pot estar buit");
+        return false;
+    }
+    console.log("Validació correcta");
+}
